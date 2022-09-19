0e24fef3c1f8c6ba3c181a0905848ab0b5990dc6
Сергей с <serega-kragov@mail.ru>
2022-09-19 17:38:35 +0300

    test change 2

--- Source.cpp
+++ Source.cpp
@@ -33,7 +33,7 @@ int main()
 {
 	using namespace sf;
 
-	const double PI = 3.141592653589793238;
+	const double PI = 3.1415926535897932384;
 	int nOfSides = getShapeN();
 	double rOutCircle = getRadius();
 	double x;
